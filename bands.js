ee80b00fb02f79af5ddf1cd2666bf7f78c710a65
John-Ryan-Johnson <john4johnkids@gmail.com>
2019-08-02 13:48:38 -0500

    initial commit

---
+++ bands.js
@@ -0,0 +1,13 @@
+const favBands = ["AC/DC", "Metallica", "ZZ Top", "Prince", "Motley Crue"]
+
+const displayBands = (bandsArray) => {
+    bandsArray.forEach(band => {
+
+      console.log(band)
+      
+    });
+
+    
+}
+
+  displayBands(favBands)
